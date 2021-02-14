7c100856bf00778ebf11a035d405f76f0af3f9e4
Susan Walker <susanwalker8@gmail.com>
2021-02-14 14:51:02 -0500

    Add more exercises

---
+++ chapter_1/ex_34.rb
@@ -0,0 +1,5 @@
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+
+# programming using ordinal not cardinal ordering
+# the index of an array can be accessed with the above notation
